8b80e579eac6c2f52e81962671f40652613dc9c2
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-07-21 23:04:45 +0330

    put input component MUI to project and onchange function

--- src/Components/InpuitTodos/Input.js
+++ src/Components/InpuitTodos/Input.js
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-
+import classes from "./Input.module.css"
+import { useState } from "react"
 export default function FullWidthTextField() {
+
+    const [todoItem , setTodoItem] = useState("")
+
+    const handleChange = (e) => {
+        setTodoItem(e.target.value)
+    }
+
+    
+
   return (
     <Box
-      sx={{
-        width: 500,
-        maxWidth: '100%',
-      }}
+      className={classes.box}
     >
-      <TextField fullWidth label="fullWidth" id="fullWidth" />
+      <TextField sx={{
+        width: 500,
+      }} 
+        label="fullWidth" 
+        id="fullWidth" 
+        onChange={handleChange}
+        />
     </Box>
   );
 }
