f1a9feb34496960373cc71bb02b9797affa2ac87
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-07-21 17:23:20 +0330

    change direction of project and material UI to RTL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Input from "./Components/InpuitTodos/Input"
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-
+      <Input/>
     </div>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Theme from './Theme/RTLTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>
 );
 
